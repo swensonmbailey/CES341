15073930f49d95ad22e7c98192910f976d717ab5
Swenson <swensonmbailey@gmail.com>
2024-05-04 14:14:29 -0500

    fix lesson2 .env

--- db/connect.js
+++ db/connect.js
@@ -9,7 +9,7 @@ const initDb = (callback) => {
     console.log('Db is already initialized!');
     return callback(null, _db);
   }
-  MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  MongoClient.connect(process.env.MONGODB_URI)
     .then((client) => {
       _db = client;
       callback(null, _db);
